637aab34c0a512a48a872828faed8c2ea7cf5594
noahbliss <noah@superuser.sh>
2020-07-14 15:57:48 -0400

    Create a2var.jl

---
+++ a2var.jl
@@ -0,0 +1,18 @@
+#Parse out equals sign delimited files easily inter julialang variables. ^.*n.x$ style. 
+# Creates a 2d array from the input file, searches for matches in the first dimension, returns value from second dimension. 
+# echo "var1=data" > settings.conf
+using DelimitedFiles
+
+settingsfile = "settings.conf"
+importedvars = readdlm(settingsfile, '=', String; skipblanks=true)
+
+function a2var(var, a)
+    c=1
+    for i in getindex(a, :, 1)
+        var == i && return getindex(a, c, 2) ; c=c+1
+    end
+    error("$var not found")
+end
+
+var1 = a2var("var1", importedvars)
+println(var1)
