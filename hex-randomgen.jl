1d90cc8afd5441d95cb19faea926b22776fbeaba
Noah Bliss <noah@superuser.sh>
2020-10-30 13:03:31 -0400

    Create hex-randomgen.jl

---
+++ hex-randomgen.jl
@@ -0,0 +1,6 @@
+#!/usr/bin/env julia
+# Noah Bliss
+# Generates a random 20 character long hex string.
+# vcat is necessary as the ranges between a-f and 0-9 are not vectors on their own. vcat combines both ranges into a single vector of all available characters. 
+Using Random
+randstring(vcat(('0':'9'),('a':'f')), 20)
